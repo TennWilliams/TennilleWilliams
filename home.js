e656637afb305694e405e7daa2ddfb5b028257cb
Tennille Williams <117609678+TennWilliams@users.noreply.github.com>
2023-02-06 10:11:03 -0500

    Add files via upload
    Separated code into JS, Style and HTML files.  Added my YouTube intro video and added meta viewport for media viewing.

---
+++ home.js
@@ -0,0 +1,6 @@
+  function greet() {
+      alert('Welcome to my page!!! Make sure to view all sections and dont forgot about my Project Page.  You can navigate there by clicking the "Project Page" button in the MIT section below or by clicking the "MIT Projects" button under the Projects tab in the Navigation Bar.  Have a wonderful day!!!');
+    }
+  setTimeout(greet, 30000); //welcome message displays after 30 seconds
+  
+
